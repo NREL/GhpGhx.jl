ef3ab31f68d086710aeed6137f3302f2ce9ddaa9
Bill Becker <William.Becker@nrel.gov>
2022-06-09 13:58:40 -0600

    Add cd("..") for Windows ccall to find file
    Instead of using relative path "../ghxmodel", need to cd first, at least for Windows

--- src/GhpGhx.jl
+++ src/GhpGhx.jl
@@ -211,7 +211,7 @@ function size_borefield(p)
                             (Ptr{Float64}, Ptr{Float64}, Ptr{Float64}, Ptr{Float64}, Ptr{Int64}, Ptr{Int64}), 
                             TimeArray, XIN, OUT, PAR, INFO, ErrorFound)
                         elseif Sys.iswindows()
-                            ccall((:type1373_, "../ghxmodel/tess_windows.so"), Cvoid, 
+                            ccall((:type1373_, "ghxmodel/tess_windows.so"), Cvoid, 
                             (Ptr{Float64}, Ptr{Float64}, Ptr{Float64}, Ptr{Float64}, Ptr{Int64}, Ptr{Int64}), 
                             TimeArray, XIN, OUT, PAR, INFO, ErrorFound)
                         end
@@ -268,7 +268,7 @@ function size_borefield(p)
                             (Ptr{Float64}, Ptr{Float64}, Ptr{Float64}, Ptr{Float64}, Ptr{Int64}, Ptr{Int64}), 
                             TimeArray, XIN, OUT, PAR, INFO, ErrorFound)
                         elseif Sys.iswindows()
-                            ccall((:type1373_, "../ghxmodel/tess_windows.so"), Cvoid, 
+                            ccall((:type1373_, "ghxmodel/tess_windows.so"), Cvoid, 
                             (Ptr{Float64}, Ptr{Float64}, Ptr{Float64}, Ptr{Float64}, Ptr{Int64}, Ptr{Int64}), 
                             TimeArray, XIN, OUT, PAR, INFO, ErrorFound)
                         end                 
@@ -489,8 +489,10 @@ function init_ghx_calls_2x!(p, TimeArray, XIN, OUT, PAR, INFO)
             ccall((:type1373_, "../ghxmodel/tess_linux.so"), Cvoid, 
             (Ptr{Float64}, Ptr{Float64}, Ptr{Float64}, Ptr{Float64}, Ptr{Int64}, Ptr{Int64}), 
             TimeArray, XIN, OUT, PAR, INFO, ErrorFound)
-        elseif Sys.iswindows()
-            ccall((:type1373_, "../ghxmodel/tess_windows.so"), Cvoid, 
+        elseif Sys.iswindows() 
+            # ccall in Windows (at least) is not able to find the relative path ("../ghxmodel"), so first have to cd("..")
+            cd("..")
+            ccall((:type1373_, "ghxmodel/tess_windows.so"), Cvoid, 
             (Ptr{Float64}, Ptr{Float64}, Ptr{Float64}, Ptr{Float64}, Ptr{Int64}, Ptr{Int64}), 
             TimeArray, XIN, OUT, PAR, INFO, ErrorFound)
         end       
@@ -504,7 +506,7 @@ function init_ghx_calls_2x!(p, TimeArray, XIN, OUT, PAR, INFO)
             (Ptr{Float64}, Ptr{Float64}, Ptr{Float64}, Ptr{Float64}, Ptr{Int64}, Ptr{Int64}), 
             TimeArray, XIN, OUT, PAR, INFO, ErrorFound)
         elseif Sys.iswindows()
-            ccall((:type1373_, "../ghxmodel/tess_windows.so"), Cvoid, 
+            ccall((:type1373_, "ghxmodel/tess_windows.so"), Cvoid, 
             (Ptr{Float64}, Ptr{Float64}, Ptr{Float64}, Ptr{Float64}, Ptr{Int64}, Ptr{Int64}), 
             TimeArray, XIN, OUT, PAR, INFO, ErrorFound)
         end        
